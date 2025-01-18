c3f70dc34d20295cbf1e319b916e2b27880f3345
Uexcel2022 <excellenceudoka@gmail.com>
2025-01-18 14:32:57 +0100

    code refactoring and  changes

---
+++ src/main/java/com/uexcel/executive/model/ReservationDates.java
@@ -0,0 +1,23 @@
+package com.uexcel.executive.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+@Entity
+@Getter
+@Setter
+@ToString
+public class ReservationDates {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private LocalDate date;
+    @ManyToOne(optional = false,cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private Reservation reservation;
+    @ManyToOne(optional = false,cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name = "roomNumber",referencedColumnName = "roomNumber",
+            foreignKey = @ForeignKey(name = "FK_RD_REGULAR"))
+    private ExecutiveRoom executiveRoom;
+}
